97010c5004f4a27193109e7714327b6a6cce1585
Silas Lenz <silas.lenz@gmail.com>
2018-12-03 11:43:16 +0100

    Day 3, Split into part 1 and 2

--- 2018/3/2.cs
+++ 2018/3/2.cs
@@ -35,15 +35,26 @@ public class HelloWorld
                 }
             }
         }
-        
-        int counter = 0;
-        for(int x = 0; x <1001; x++){
-            for (int y = 0; y < 1001; y++){
-                if (fabric_usage[x,y]>1)
-                    counter++;
+
+        foreach(string line in lines){  
+            string[] words = line.Split(' ');
+            string id = words[0];
+            Int32[] start_coordinates = words[2].Trim(':').Split(',').Select(s => Int32.Parse(s)).ToArray();
+            Int32[] size = words[3].Split('x').Select(s => Int32.Parse(s)).ToArray();;
+
+            bool is_unique = true;
+
+            for(int x = start_coordinates[0]; x <start_coordinates[0]+size[0]; x++){
+                for (int y = start_coordinates[1]; y < start_coordinates[1]+size[1]; y++){
+                    if (fabric_usage[x,y] != 1){
+                        is_unique = false;
+                    }
+                }
+            }
+            if (is_unique){
+                System.Console.WriteLine(id);
+                break;
             }
         }
-
-        System.Console.WriteLine(counter);
     }
 }
