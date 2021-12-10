9019864622a756354648a64186b405b39b1b9561
Silas Lenz <silas.lenz@gmail.com>
2021-12-10 13:35:25 +0100

    Add day 1+2 for 2021

---
+++ 2021/AdventOfCode2021-1/AdventOfCode2021-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+Console.WriteLine("Hello, World!");
+
+var numbers = new List<int>();
+foreach (string line in File.ReadLines("input"))
+{
+    numbers.Add(Int32.Parse(line));
+}
+
+var lastNum = Int32.MaxValue;
+var numberOfIncreases = 0;
+foreach (var num in numbers)
+{
+    if (num > lastNum)
+    {
+        numberOfIncreases++;
+    }
+
+    lastNum = num;
+}
+
+Console.WriteLine(numberOfIncreases);
+
+numberOfIncreases = 0;
+for (var index = 0; index < numbers.Count - 3; index++)
+{
+    int threeSum = numbers[index] + numbers[index + 1] + numbers[index + 2];
+    int nextThreeSum = numbers[index + 1] + numbers[index + 2] + numbers[index + 3];
+    if (nextThreeSum > threeSum)
+    {
+        numberOfIncreases++;
+    }
+}
+
+Console.WriteLine(numberOfIncreases);
---
+++ 2021/AdventOfCode2021-2/AdventOfCode2021-2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+var numbers = new List<int>();
+var position = new Point();
+foreach (string line in File.ReadLines("input"))
+{
+    var split_line = line.Split(" ");
+    switch (split_line[0])
+    {
+        case "forward":
+            position.Horizontal+=int.Parse(split_line[1]);
+            break;
+        case "down":
+            position.Depth+=int.Parse(split_line[1]);
+            break;
+        case "up":
+            position.Depth-=int.Parse(split_line[1]);
+            break;
+    }
+}
+Console.WriteLine($"{position.Depth} {position.Horizontal}");
+Console.WriteLine(position.Depth*position.Horizontal);
+
+position.Horizontal = 0;
+position.Depth = 0;
+foreach (string line in File.ReadLines("input"))
+{
+    var split_line = line.Split(" ");
+    switch (split_line[0])
+    {
+        case "forward":
+            position.Horizontal+=int.Parse(split_line[1]);
+            position.Depth+=(int.Parse(split_line[1])*position.Aim);
+            break;
+        case "down":
+            position.Aim += int.Parse(split_line[1]);
+            break;
+        case "up":
+            position.Aim -= int.Parse(split_line[1]);
+            break;
+    }
+}
+Console.WriteLine($"{position.Depth} {position.Horizontal}");
+Console.WriteLine(position.Depth*position.Horizontal);
+public struct Point
+{
+    public int Horizontal{ get; set; }
+    public int Depth { get; set; }
+    public int Aim { get; set; }
+};
