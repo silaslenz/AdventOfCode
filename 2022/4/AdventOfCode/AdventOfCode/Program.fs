92467355e38254efebd7875ca7f7a9b1d6609753
Silas Lenz <silas.lenz@gmail.com>
2022-12-09 22:27:21 +0100

    Implement 2022 day 4 part 2

--- 2022/4/AdventOfCode/AdventOfCode/Program.fs
+++ 2022/4/AdventOfCode/AdventOfCode/Program.fs
@@ -1,15 +1,24 @@
 ﻿let lines = System.IO.File.ReadAllLines("input")
 let counter = ref 0
+let counterTwo = ref 0
 
 for line in lines do
     let split = line.Split(',')
     let a = split[ 0 ].Split('-') |> Array.map int
     let b = split[ 1 ].Split('-') |> Array.map int
 
-    if
-        (Set.ofSeq (seq { a[0] .. a[1] }) - Set.ofSeq (seq { b[0] .. b[1] })).Count = 0
-        || (Set.ofSeq (seq { b[0] .. b[1] }) - Set.ofSeq (seq { a[0] .. a[1] })).Count = 0
-    then
+    let idsInAButNotB =
+        Set.ofSeq (seq { a[0] .. a[1] }) - Set.ofSeq (seq { b[0] .. b[1] })
+
+    let idsInBButNotA =
+        (Set.ofSeq (seq { b[0] .. b[1] }) - Set.ofSeq (seq { a[0] .. a[1] }))
+
+    if idsInAButNotB.Count = 0 || idsInBButNotA.Count = 0 then
         counter.Value <- counter.Value + 1
 
-printfn $"{counter.Value}"
+    if idsInAButNotB.Count < a[1] - a[0] + 1 || idsInBButNotA.Count < b[1] - b[0] + 1 then
+        counterTwo.Value <- counterTwo.Value + 1
+
+
+printfn $"Part 1: {counter.Value}"
+printfn $"Part 2: {counterTwo.Value}"
